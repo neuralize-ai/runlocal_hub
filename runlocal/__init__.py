d137d11c72a9aafa8fdf074b87e9e8bfc8fbbadf
ciaranbor <ciaranborourke-dev@proton.me>
2025-06-02 16:35:12 +0100

    Add classes to runlocal/__init__.py

--- runlocal/__init__.py
+++ runlocal/__init__.py
@@ -1,1 +1,26 @@
-# RunLocal API Client Package
+"""RunLocal API Client Package"""
+
+from .client import RunLocalClient
+from .models.device import Device, DeviceUsage
+from .models.job import JobType, JobResult
+from .models.benchmark import BenchmarkData, BenchmarkStatus
+from .models.tensor import IOType, TensorInfo
+from .exceptions import RunLocalError, ConfigurationError, ValidationError, UploadError
+
+__version__ = "0.1.0"
+__all__ = [
+    "RunLocalClient",
+    "Device",
+    "DeviceUsage",
+    "JobType",
+    "JobResult",
+    "IOType",
+    "TensorInfo",
+    "BenchmarkData",
+    "BenchmarkStatus",
+    "RunLocalError",
+    "ConfigurationError",
+    "ValidationError",
+    "UploadError",
+]
+
