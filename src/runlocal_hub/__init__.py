a91e7215547789d50ccdcd1226f2c9c00439ddb6
ciaranbor <ciaranborourke-dev@proton.me>
2025-06-09 19:09:01 +0100

    Use dynamic versioning with __version__ file

--- src/runlocal_hub/__init__.py
+++ src/runlocal_hub/__init__.py
@@ -1,16 +1,29 @@
 """RunLocal API Client Package"""
 
+from .__version__ import __version__
 from .client import RunLocalClient
 from .models.device import Device, DeviceUsage
 from .models.job import JobType, JobResult
 from .models.benchmark import BenchmarkData, BenchmarkStatus
 from .models.tensor import IOType, TensorInfo
 from .devices.filters import DeviceFilters
-from .exceptions import RunLocalError, ConfigurationError, ValidationError, UploadError
+from .exceptions import (
+    RunLocalError,
+    AuthenticationError,
+    APIError,
+    ModelNotFoundError,
+    DeviceNotAvailableError,
+    JobTimeoutError,
+    TensorError,
+    UploadError,
+    ValidationError,
+    NetworkError,
+    ConfigurationError,
+)
 from .utils.display import display_benchmark_results, display_failed_benchmarks
 
-__version__ = "0.1.0"
 __all__ = [
+    "__version__",
     "RunLocalClient",
     "Device",
     "DeviceUsage",
@@ -22,9 +35,16 @@ __all__ = [
     "BenchmarkData",
     "BenchmarkStatus",
     "RunLocalError",
-    "ConfigurationError",
-    "ValidationError",
+    "AuthenticationError",
+    "APIError",
+    "ModelNotFoundError",
+    "DeviceNotAvailableError",
+    "JobTimeoutError",
+    "TensorError",
     "UploadError",
+    "ValidationError",
+    "NetworkError",
+    "ConfigurationError",
     "display_benchmark_results",
     "display_failed_benchmarks",
 ]
