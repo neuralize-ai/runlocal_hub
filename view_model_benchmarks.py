8f860a6c867985f66ee19bd05c04574e1c04fc6e
ciaranbor <ciaranborourke-dev@proton.me>
2025-07-10 16:57:37 +0100

    Show model info in view benchmarks example

--- view_model_benchmarks.py
+++ view_model_benchmarks.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
-from runlocal_hub import RunLocalClient, display_benchmark_results
+from runlocal_hub import RunLocalClient, display_benchmark_results, display_model
 
 
 def main():
     client = RunLocalClient()
 
-    model_ids = client.get_models_ids()
+    model_id = client.get_models_ids()[0]
 
-    benchmark_data = client.get_model_benchmarks(model_ids[0])
+    model_info = client.get_model(model_id)
+
+    display_model(model_info)
+
+    benchmark_data = client.get_model_benchmarks(model_id)
 
     display_benchmark_results(benchmark_data, show_ram_usage=True)
 
