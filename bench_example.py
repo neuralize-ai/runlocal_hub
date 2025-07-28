abdf539cebd432dd5d7f02cf0a9063d4a4919c1a
ciaranbor <ciaranborourke-dev@proton.me>
2025-07-28 20:03:01 +0100

    Update bench example to use settings

--- bench_example.py
+++ bench_example.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
-from runlocal_hub import DeviceFilters, RunLocalClient, display_benchmark_results
+from runlocal_hub import (
+    RuntimeSettings,
+    DeviceFilters,
+    RunLocalClient,
+    display_benchmark_results,
+)
+
+from runlocal_hub.models.settings.coreml import CoreMLSettings, SpecializationStrategy
 
 
 def main():
@@ -9,13 +16,23 @@ def main():
     model_path = "model.mlpackage"
 
     device_filters = DeviceFilters(
-        device_name="MacBook",  # Filter by device name
+        # device_name="MacBook",  # Filter by device name
         year_min=2023,
     )
+
+    settings = RuntimeSettings()
+
+    coreml_settings = CoreMLSettings(
+        allowLowPrecisionAccumulationOnGPU=True,
+        specializationStrategy=SpecializationStrategy.fastPrediction,
+    )
+
+    settings.framework_settings = coreml_settings.format()
 
     try:
         result = client.benchmark(
             model_path=model_path,
+            settings=settings,
             device_filters=device_filters,
             timeout=None,  # Block until finished
         )
